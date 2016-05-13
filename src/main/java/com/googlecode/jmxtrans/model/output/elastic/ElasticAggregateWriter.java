0de7de41ab24249e19e8507e8adbb04185878fc2
Vasco Veloso <vveloso@gmail.com>
2016-05-13 08:33:08 +0100

    Initial commit.

---
+++ src/main/java/com/googlecode/jmxtrans/model/output/elastic/ElasticAggregateWriter.java
@@ -0,0 +1,143 @@
+package com.googlecode.jmxtrans.model.output.elastic;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.googlecode.jmxtrans.exceptions.LifecycleException;
+import com.googlecode.jmxtrans.model.Query;
+import com.googlecode.jmxtrans.model.Result;
+import com.googlecode.jmxtrans.model.Server;
+import com.googlecode.jmxtrans.model.ValidationException;
+import com.googlecode.jmxtrans.model.output.BaseOutputWriter;
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.core.DocumentResult;
+import io.searchbox.core.Index;
+import org.apache.http.annotation.NotThreadSafe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.google.common.base.Optional.fromNullable;
+import static com.google.common.base.Strings.isNullOrEmpty;
+
+/**
+ * Created by vveloso on 12-05-2016.
+ */
+@NotThreadSafe
+public class ElasticAggregateWriter extends BaseOutputWriter {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ElasticAggregateWriter.class);
+
+	private static final String DEFAULT_TYPE_NAME = "jmx-entry";
+	private static final String DEFAULT_INDEX_NAME = "jmxtrans";
+
+	private final String elasticTypeName;
+	private final String elasticIndexName;
+	private final JestClient jestClient;
+
+	@JsonCreator
+	public ElasticAggregateWriter(@JsonProperty("typeNames") ImmutableList<String> typeNames,
+								  @JsonProperty("booleanAsNumber") boolean booleanAsNumber,
+								  @JsonProperty("debug") Boolean debugEnabled,
+								  @JsonProperty("connectionUrl") String connectionUrl,
+								  @JsonProperty("elasticTypeName") String elasticTypeName,
+								  @JsonProperty("elasticIndexName") String elasticIndexName,
+								  @JsonProperty("settings") Map<String, Object> settings) {
+		super(typeNames, booleanAsNumber, debugEnabled, settings);
+		final Map<String, Object> settingsMap = MoreObjects.firstNonNull(settings, Collections.emptyMap());
+
+		this.elasticIndexName = firstNonNull(elasticIndexName, (String) settingsMap.get("elasticIndexName"), DEFAULT_INDEX_NAME);
+		this.elasticTypeName = firstNonNull(elasticTypeName, (String) settingsMap.get("elasticTypeName"), DEFAULT_TYPE_NAME);
+
+		this.jestClient = createJestClient(connectionUrl);
+	}
+
+	private JestClient createJestClient(String connectionUrl) {
+		LOGGER.info("Creating a jest elastic search client for connection URL: {}", connectionUrl);
+		JestClientFactory factory = new JestClientFactory();
+		factory.setHttpClientConfig(
+				new HttpClientConfig.Builder(connectionUrl)
+						.multiThreaded(true)
+						.build());
+		return factory.getObject();
+	}
+
+	@Override
+	public void start() throws LifecycleException {
+		super.start();
+
+	}
+
+	@Override
+	public void stop() throws LifecycleException {
+		super.stop();
+
+		jestClient.shutdownClient();
+	}
+
+	public void validateSetup(Server server, Query query) throws ValidationException {
+		// nothing to validate
+	}
+
+	@Override
+	protected void internalWrite(Server server, Query query, ImmutableList<Result> results) throws Exception {
+		if (results.isEmpty()) {
+			LOGGER.debug("Not processing empty query result.");
+			return;
+		}
+
+		final Map<String, List<Result>> resultsMap = results.stream()
+				.collect(Collectors.groupingBy(Result::getTypeName));
+
+		LOGGER.debug("Query results: {}", resultsMap);
+
+		resultsMap.forEach((t, r) -> write(server, t, r));
+	}
+
+	private void write(Server server, String typeName, List<Result> results) {
+		final Map<String, Object> document = new HashMap<>();
+		final Map<String, Object> metadata = new HashMap<>(5);
+		final String typeNameValues = getConcatedTypeNameValues(typeName);
+
+		metadata.put("serverAlias", server.getAlias());
+		metadata.put("server", server.getHost());
+		metadata.put("port", server.getPort());
+		metadata.put("typeName", typeName);
+		metadata.put("typeNameValues", typeNameValues);
+
+		document.put("@metadata", metadata);
+
+		for (final Result result : results) {
+			document.putIfAbsent("@timestamp", result.getEpoch());
+
+			final ImmutableMap<String, Object> values = result.getValues();
+			document.put(
+					fromNullable(result.getKeyAlias()).or(result.getAttributeName()),
+					values.size() == 1 ? values.get(result.getAttributeName()) : values);
+		}
+
+		LOGGER.debug("Insert into Elastic index [{}] with type [{}]: {}", elasticIndexName, elasticTypeName, document);
+
+		final Index idx = new Index.Builder(document).index(elasticIndexName).type(elasticTypeName).build();
+		try {
+			final DocumentResult result = jestClient.execute(idx);
+			if (!result.isSucceeded()) {
+				LOGGER.error("Failed to write entry to Elastic: {}", result.getErrorMessage());
+			}
+		} catch (IOException e) {
+			LOGGER.error("Failed to write entry to Elastic: {}", e);
+		}
+	}
+
+}
