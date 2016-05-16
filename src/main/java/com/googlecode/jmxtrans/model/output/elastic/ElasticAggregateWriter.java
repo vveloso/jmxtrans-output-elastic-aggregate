981d73e7fce63137f4b81acccc8c3f5db9b46767
Vasco Veloso <vveloso@gmail.com>
2016-05-16 16:04:02 +0100

    Enhanced class documentation.

--- src/main/java/com/googlecode/jmxtrans/model/output/elastic/ElasticAggregateWriter.java
+++ src/main/java/com/googlecode/jmxtrans/model/output/elastic/ElasticAggregateWriter.java
@@ -3,7 +3,6 @@ package com.googlecode.jmxtrans.model.output.elastic;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.MoreObjects;
-import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.googlecode.jmxtrans.exceptions.LifecycleException;
@@ -26,10 +25,28 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.google.common.base.Optional.fromNullable;
-import static com.google.common.base.Strings.isNullOrEmpty;
 
 /**
- * Created by vveloso on 12-05-2016.
+JMXTrans output writer for Elasticsearch which outputs one structured document for each query.
+Documents pushed to Elastic have two automatically generated attributes, @metadata and @timestamp. An example of metrics obtained for a JMS queue would be:
+<pre>
+ {
+ 	"@timestamp": 1463071268789,
+ 	"@metadata": {
+ 		"typeName": "module=Core,type=Queue,address=\"jms.queue.nb8Queue\",name=\"jms.queue.nb8Queue\"",
+ 		"server": "server.example.com",
+ 		"typeNameValues": "Queue_\"jms.queue.nb8Queue\"_\"jms.queue.nb8Queue\"",
+ 		"port": "9990"
+ 	},
+ 	"MessagesAdded": 0,
+ 	"Temporary": false,
+ 	"ConsumerCount": 20,
+ 	"DeliveringCount": 0,
+ 	"Durable": false,
+ 	"MessageCount": 0,
+ 	"ScheduledCount": 0
+ }
+</pre>
  */
 @NotThreadSafe
 public class ElasticAggregateWriter extends BaseOutputWriter {
